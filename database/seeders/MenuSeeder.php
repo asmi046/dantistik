8b887477f55ecd84615ba848ddb01fe7e2fba62d
asmi046 <asmi046@gmail.com>
2026-02-12 15:33:49 +0300

    Первые 3 секции

---
+++ database/seeders/MenuSeeder.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class MenuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'menu_name' => 'Главное меню',
+                'title' => 'Специалисты',
+                'order' => 1,
+                'lnk' => '#',
+            ],
+            [
+                'menu_name' => 'Главное меню',
+                'title' => 'Услуги',
+                'order' => 1,
+                'lnk' => '#',
+            ],
+            [
+                'menu_name' => 'Главное меню',
+                'title' => 'Цены',
+                'order' => 1,
+                'lnk' => '#',
+            ],
+            [
+                'menu_name' => 'Главное меню',
+                'title' => 'Контакты',
+                'order' => 1,
+                'lnk' => '#',
+            ],
+        ];
+
+        DB::table('menus')->insert($data);
+
+        // $footer_menu = [
+        //     [
+        //         'menu_name' => 'Меню в подвале',
+        //         'title' => 'О компании',
+        //         'order' => 1,
+        //         'lnk' => "#",
+        //     ],
+        //     [
+        //         'menu_name' => 'Меню в подвале',
+        //         'title' => 'Цены',
+        //         'order' => 2,
+        //         'lnk' => "/price",
+        //     ],
+        //     [
+        //         'menu_name' => 'Меню в подвале',
+        //         'title' => 'Сваи',
+        //         'order' => 3,
+        //         'lnk' => "'/products",
+        //     ],
+        //     [
+        //         'menu_name' => 'Меню в подвале',
+        //         'title' => 'Галерея',
+        //         'order' => 4,
+        //         'lnk' => "#",
+        //     ],
+        //     [
+        //         'menu_name' => 'Меню в подвале',
+        //         'title' => 'FAQ',
+        //         'order' => 7,
+        //         'lnk' => "/faq",
+        //     ],
+        // ];
+
+        // DB::table("menus")->insert($footer_menu);
+
+        // $footer_menu_2 = [
+
+        //     [
+        //         'menu_name' => 'Полезная информация',
+        //         'title' => 'Акции',
+        //         'order' => 5,
+        //         'lnk' => "/sales",
+        //     ],
+        //     [
+        //         'menu_name' => 'Полезная информация',
+        //         'title' => 'Отзывы',
+        //         'order' => 6,
+        //         'lnk' => "/review",
+        //     ],
+        //     [
+        //         'menu_name' => 'Полезная информация',
+        //         'title' => 'Оплата и доставка',
+        //         'order' => 8,
+        //         'lnk' => "#",
+        //     ],
+        //     [
+        //         'menu_name' => 'Полезная информация',
+        //         'title' => 'Сертификаты',
+        //         'order' => 9,
+        //         'lnk' => "/certificat",
+        //     ],
+        //     [
+        //         'menu_name' => 'Полезная информация',
+        //         'title' => 'Контакты',
+        //         'order' => 10,
+        //         'lnk' => "/contacts",
+        //     ],
+        // ];
+
+        // DB::table("menus")->insert($footer_menu_2);
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,4 +18,66 @@
         </div>
     </section>
 
+    <section class="logo-menu-header" id="logo-menu-header">
+        <div class="container logo-menu-header__container">
+            <a class="logo-menu-header__logo" href="{{ route('home') }}">
+                <img src="{{ asset('img/logo.svg') }}" alt="Логотип" />
+            </a>
+
+            <div class="logo-menu-header__menu">
+                <x-menues.puncts></x-menues.puncts>
+            </div>
+
+            <a class="button" href="#">Заказать звонок</a>
+        </div>
+    </section>
+
+    <section class="main-banner" id="main-banner">
+        <video class="main-banner__video" autoplay muted loop playsinline preload="auto"
+            poster="{{ asset('img/main_b.webp') }}">
+            <source src="{{ asset('video/main_video.mp4') }}" type="video/mp4" />
+        </video>
+
+        <div class="container">
+            <div class="main-banner__content">
+                <h1>Современная <strong>стоматология в Курске</strong></h1>
+                <p>Новейшее оборудование, передовые технологии опытные врачи</p>
+                <a class="button" href="#">Заказать звонок</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="about-company" id="about-company">
+        <div class="container">
+            <div class="about-company__inner">
+                <div class="about-company__left">
+                    <h2>Стоматологическая клиника
+                        <strong>ДАНТИСТиК</strong>
+                    </h2>
+
+                    <div class="about-company__text">
+                        {!! file_get_contents(public_path('tmp_data/about.html')) !!}
+                    </div>
+                </div>
+
+                <div class="about-company__right">
+                    <div class="about-company__stats">
+                        <div class="about-company__stat">
+                            <div class="about-company__stat-number">15+</div>
+                            <div class="about-company__stat-text">Лет дарим здоровую улыбку нашим пациентам</div>
+                        </div>
+                        <div class="about-company__stat">
+                            <div class="about-company__stat-number">3578</div>
+                            <div class="about-company__stat-text">Давольных пациентов за время работы клиники</div>
+                        </div>
+                        <div class="about-company__stat">
+                            <div class="about-company__stat-number">95</div>
+                            <div class="about-company__stat-text">Дипломов и сертификатов у наших специалистов</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
